d1cd21f23d9963144a3e4d21040dcf1b72db6b5d
vahap <demir.vahap@hotmail.com>
2017-12-21 00:11:35 +0000

    login html changed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
 import { IConfig, ConfigService } from '../app.config';
 import { LoginServiceService } from '../shared/services/login.services';
+import { User } from '../shared/DTOs/user';
 
 @Component({
     selector: 'app-login',
@@ -16,31 +17,36 @@ export class LoginComponent implements OnInit {
     userDetail: any;
     config: IConfig;
     errorMessage: string;
-    user: any;
+    public user: User;
 
-    constructor(private loginService: LoginServiceService,private configService: ConfigService,public router: Router) {
-      //  this.user = { userName: "vahap", Password: "demir" };
-      this.user={};
+
+    constructor(private loginService: LoginServiceService, private configService: ConfigService, public router: Router) {
+        // this.user = { userName: "vahap", password: "demir" };
+        this.user = new User();
+       
     }
 
     ngOnInit() {
         this.config = this.configService.getAppConfig();
+        this.errorMessage="test test";
     }
 
     onLoggedin() {
         console.log(this.user);
+
         this.loginService.login(this.config.logInUrl, this.user)
-        .subscribe(items => {
-          if (items != null) {         
-            // this.userDetail = this.dataSharingService.setUserQuery(items);
-            localStorage.setItem('isLoggedin', 'true');
-            this.router.navigate(['/dashboard']);
+            .subscribe(items => {
+                if (items.length != 0) {
+                    // this.userDetail = this.dataSharingService.setUserQuery(items);
+                    localStorage.setItem('isLoggedin', 'true');
+                    this.router.navigate(['/dashboard']);
 
-          }
-        },
-        error => this.errorMessage = <any>error
+                }
+                error => this.errorMessage = <any>error
+            },
+            error => this.errorMessage = <any>error
 
-        );
-    
+            );
+
     }
 }
