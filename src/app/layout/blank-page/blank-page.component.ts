dcb6fde986a6c1edbc6f98022f9e17900f6507b9
vahap <demir.vahap@hotmail.com>
2018-01-08 23:22:06 +0000

    Pdf example added to blank page

--- src/app/layout/blank-page/blank-page.component.ts
+++ src/app/layout/blank-page/blank-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, Inject } from '@angular/core';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { BlankPageService } from './blank-page.service';
 import { ConfigService, IConfig } from '../../app.config';
@@ -7,11 +7,15 @@ import { CommonService } from '../../shared/common.service';
 import { brand } from '../../shared/DTOs/brand';
 import { category } from '../../shared/DTOs/category';
 import { NgForm }    from '@angular/forms';
+import * as jsPDF from 'jspdf'
 //This component and its service have been created for testing porpuse.
 @Component({
     selector: 'app-blank-page',
     templateUrl: './blank-page.component.html',
-    styleUrls: ['./blank-page.component.scss']
+    styleUrls: ['./blank-page.component.scss'],
+    providers: [
+        { provide: 'Window',  useValue: window }
+      ]
 })
 export class BlankPageComponent implements OnInit {
     config: IConfig;
@@ -22,10 +26,23 @@ export class BlankPageComponent implements OnInit {
     selectedCategory: category;
     model: any=[];
     items=[];
-    constructor(private commonServices: CommonService, private blankPageServices: BlankPageService, private configService: ConfigService, public toastr: ToastsManager, vcr: ViewContainerRef) {
+    constructor( @Inject('Window') private window: Window,private commonServices: CommonService, private blankPageServices: BlankPageService, private configService: ConfigService, public toastr: ToastsManager, vcr: ViewContainerRef) {
         this.toastr.setRootViewContainerRef(vcr);
         this.model = { username: '', password: '' };
     }
+
+    
+  download() {
+
+    var doc = new jsPDF();
+    doc.text(20, 20, 'Hello Kenan Gardas!');
+    doc.text(20, 30, '20 lira ver senin icin adam picakliyim');
+    doc.addPage();
+    doc.text(20, 20, 'test');
+
+    // Save the PDF
+    doc.save('ConanLtdSiparisler.pdf');
+}
     ngOnInit() {
         this.config = this.configService.getAppConfig();
         this.getCategories();
