700eef8a3888f5b89479212916456a197b610848
Abdulvahap Demir <demir.vahap@gmail.com>
2018-02-28 12:07:30 +0000

    edit waybill  before refactoring.!

--- src/app/layout/waybills/new-waybill/new-waybill.component.ts
+++ src/app/layout/waybills/new-waybill/new-waybill.component.ts
@@ -21,278 +21,242 @@ import { product } from '../../../shared/DTOs/product';
 })
 export class NewWaybillComponent implements OnInit {
   config: IConfig;
-  basketProducts:BasketProduct[]=[];
-  currentWaybill:BasketProduct[]=[];
-  customers:customer[]=[];
-  selectedCustomer:customer=new customer();
-  selectedAddress:address=new address();
-  originalWaybill:Waybill=new Waybill();
-  deletedBasketProducts:BasketProduct[]=[];
-  selectedDate:Date;
-  productList:product[]=[];
+  basketProducts: BasketProduct[] = [];
+  currentWaybill: BasketProduct[] = [];
+  customers: customer[] = [];
+  selectedCustomer: customer = new customer();
+  selectedAddress: address = new address();
+  deletedBasketProducts: BasketProduct[] = [];
+  selectedDate: Date;
+  productList: product[] = [];
+  loading: boolean;
   @Input()
   selectedWayBill: Waybill;
 
-  constructor(private customerService:CustomersService,public toastr: ToastsManager, vcr: ViewContainerRef, private waybillService:WaybillService, private productsService:ProductsService,private configService: ConfigService,public dialog: MatDialog) 
-  {
+  constructor(private customerService: CustomersService, public toastr: ToastsManager, vcr: ViewContainerRef, private waybillService: WaybillService, private productsService: ProductsService, private configService: ConfigService, public dialog: MatDialog) {
     this.toastr.setRootViewContainerRef(vcr);
-    
-   }
+    this.loading = false;
+  }
 
-   ngOnChanges()
-   {
-    if(this.selectedWayBill!=null){
- 
-     this.getWayBillById(this.selectedWayBill.id);
-     this.fillBasketProducts();
-      }
-   }
+  ngOnChanges() {
+    if (this.selectedWayBill != null) {
+      this.getWayBillById(this.selectedWayBill.id);
+      this.fillBasketProducts();
+    }
+  }
 
   ngOnInit() {
     this.config = this.configService.getAppConfig();
-    this.currentWaybill=[];
+    this.currentWaybill = [];
+    this.deletedBasketProducts = [];
     this.getProducts();
-  //  this.fillBasketProducts();
+    //  this.fillBasketProducts();
     this.fillCurrentWaybill();
     this.fillCustomers();
-   // this.setLastWaybill();
-
+    // this.setLastWaybill();
   }
 
   getWayBillById(selectedWayBillId): any {
-  //  this.loading = true;
 
+    this.loading = true;
     this.waybillService.getWaybill(this.config.getWaybillUrl, selectedWayBillId)
       .subscribe(items => {
-        this.currentWaybill=[];
-      var wayBill=items;
-      this.originalWaybill= { ...items };
-      this.selectedCustomer=this.customers.filter(x => x.id == wayBill.customer.id)[0];
-      this.selectedCustomer.addresses=wayBill.customer.addresses;
-      this.selectedAddress= wayBill.customer.addresses.filter(x => x.id == wayBill.addressId)[0];
-     this.selectedDate= new Date(wayBill.waybillDate);
-      wayBill.waybillProducts.forEach(wp => {
-        var product=this.productList.filter(x=>x.id==wp.productId)[0];
-        let basketProduct=new BasketProduct();
-        basketProduct.id=wp.id;
-        basketProduct.waybillId=selectedWayBillId;
-        basketProduct.product=product;
-        basketProduct.package=wp.numberOfPackage;
- 
-        this.addProductToCurrentWaybill(basketProduct);
-
-      });
-      //    this.loading = false;
-    //    }
+        this.currentWaybill = [];
+        this.deletedBasketProducts = [];
+        var wayBill = items;
+        this.selectedCustomer = this.customers.filter(x => x.id == wayBill.customer.id)[0];
+        this.selectedCustomer.addresses = wayBill.customer.addresses;
+        this.selectedAddress = wayBill.customer.addresses.filter(x => x.id == wayBill.addressId)[0];
+        this.selectedDate = new Date(wayBill.waybillDate);
+        wayBill.waybillProducts.forEach(wp => {
+          var product = this.productList.filter(x => x.id == wp.productId)[0];
+          let basketProduct = new BasketProduct();
+          basketProduct.id = wp.id;
+          basketProduct.waybillId = selectedWayBillId;
+          basketProduct.product = product;
+          basketProduct.package = wp.numberOfPackage;
+          this.addProductToCurrentWaybill(basketProduct);
+        });
       },
-      error => this.toastr.error('Siparisler getirilirken hata ile karsilasildi.'+error, 'Error!'),
-      () => {
-        //finally bloke ..!
-        this.fillBasketProducts();
-      }
+        error => this.toastr.error('Irsaliye getirilirken hata ile karsilasildi.' + error, 'Error!'),
+        () => {
+          this.loading = false;
+          this.loading
+          this.fillBasketProducts();
+        }
       );
   }
 
-  setLastWaybill()
-  {
+  setLastWaybill() {
     // this.waybillService.getLastWaybill(this.config.getLastWaybillUrl,null).subscribe(result=>{
     //   this.lastWaybill=result;
     // });
   }
 
-  fillCustomers()
-  {
-    this.customerService.getCustomers(this.config.getCustomersUrl,null).subscribe(result=>
-      {
-
-        this.customers=result;
-
-      });
+  fillCustomers() {
+    this.customerService.getCustomers(this.config.getCustomersUrl, null).subscribe(result => {
+      this.customers = result;
+    });
   }
-  increase(basketProduct:BasketProduct)
-  {
+  increase(basketProduct: BasketProduct) {
     basketProduct.package++;
-    basketProduct.status="edited";
-   this.addProductToCurrentWaybill(basketProduct);
+    basketProduct.status = "edited";
+    this.addProductToCurrentWaybill(basketProduct);
   }
 
+  decrease(basketProduct: BasketProduct) {
 
-    decrease(basketProduct: BasketProduct) {
-    
-      if (basketProduct.package <= 1) {//remove product from basket
-        let updateProduct= this.basketProducts.filter(x=>x.product==basketProduct.product)[0];
-        updateProduct.package=0;
-        basketProduct.package = 0;
-        this.removeProductToCurrentWaybill(basketProduct);
-      }
-      else {
-        basketProduct.package -= 1;
-        basketProduct.status="edited";
-        this.addProductToCurrentWaybill(basketProduct);
-      }
-     // this.getProductsInBasket();
-  
+    if (basketProduct.package <= 1) {//remove product from basket
+      let updateProduct = this.basketProducts.filter(x => x.product == basketProduct.product)[0];
+      updateProduct.package = 0;
+      basketProduct.package = 0;
+      this.removeProductToCurrentWaybill(basketProduct);
     }
-  
-
-  saveCurrentWaybillProducts(waybillProducts:WaybillProduct[])
-  {
-   // localStorage.setItem("currentWaybill",JSON.stringify(waybillProducts));
+    else {
+      basketProduct.package -= 1;
+      basketProduct.status = "edited";
+      this.addProductToCurrentWaybill(basketProduct);
+    }
+    // this.getProductsInBasket();
   }
 
-  fillCurrentWaybill() 
-  {
+  saveCurrentWaybillProducts(waybillProducts: WaybillProduct[]) {
+    // localStorage.setItem("currentWaybill",JSON.stringify(waybillProducts));
+  }
+
+  fillCurrentWaybill() {
     //this.currentWaybill=JSON.parse(localStorage.getItem("currentWaybill")) || [];
-  
   }
 
- clearProducts()
- {
-  let dialogRef = this.dialog.open(ConfirmComponent, {
-    width: '50%',
-    data: { title:"irsaliyeyi sıfırlamak istiyor musunuz?" }
-  });
 
-  dialogRef.afterClosed().subscribe(result => {
-   if(result=='yes')
-   {
-    this.removeCurrentWaybill();
-   }
-  });
- }
-  removeCurrentWaybill()
-  {
-   // localStorage.removeItem("currentWaybill");
-    this.currentWaybill=[];  
+  removeCurrentWaybill() {
+    // localStorage.removeItem("currentWaybill");
+    this.currentWaybill = [];
   }
 
-  addProductToCurrentWaybill(basketProduct:BasketProduct)
-  {
+  addProductToCurrentWaybill(basketProduct: BasketProduct) {
     let isExist = false;
- 
-   let updateProduct= this.basketProducts.filter(x=>x.product==basketProduct.product)[0];
-   updateProduct.package=basketProduct.package;
+    let updateProduct = this.basketProducts.filter(x => x.product == basketProduct.product)[0];
+    updateProduct.package = basketProduct.package;
 
     for (let i = 0; i < this.currentWaybill.length; i++)//check if product exist in currentWaybill
     {
-      
       if (this.currentWaybill[i].product.id == basketProduct.product.id) {
         this.currentWaybill[i].package = basketProduct.package;
+        this.currentWaybill[i].status = basketProduct.status;
         isExist = true;
         break;
-       
       }
-      
     }
-    
-    if (!isExist) {
-     // basketProduct.status="added";
-       this.currentWaybill=[...this.currentWaybill,basketProduct];
-      
-      }
 
- //   localStorage.setItem("currentWaybill", JSON.stringify(this.currentWaybill));
+    if (!isExist) {
+      // basketProduct.status="added";
+      this.currentWaybill = [...this.currentWaybill, basketProduct];
+    }
+    //   localStorage.setItem("currentWaybill", JSON.stringify(this.currentWaybill));
   }
 
   removeProductToCurrentWaybill(basketProduct: BasketProduct) {
     for (let i = 0; i < this.currentWaybill.length; i++)//check if product exist in basket
     {
-      
       if (this.currentWaybill[i].product.id == basketProduct.product.id) {
         var index = this.currentWaybill.indexOf(this.currentWaybill[i]);
         if (index > -1) {
-          basketProduct.status="deleted";
-          this.deletedBasketProducts.push(basketProduct)
+          //if any existing product deleted 
+          if (this.currentWaybill[i].id != null && this.deletedBasketProducts.indexOf(this.currentWaybill[i]) < 0) {
+            this.currentWaybill[i].status = "deleted";
+            this.deletedBasketProducts.push(this.currentWaybill[i])
+          }
           this.currentWaybill.splice(index, 1);
-          this.currentWaybill=[...this.currentWaybill];
+          this.currentWaybill = [...this.currentWaybill];
+        }
       }
-      }
-      
     }
-
-   // localStorage.setItem("currentWaybill", JSON.stringify(this.currentWaybill));
+    // localStorage.setItem("currentWaybill", JSON.stringify(this.currentWaybill));
   }
 
-  getProductFromCurrentWaybill(id:number)
-  {
+  getProductFromCurrentWaybill(id: number) {
     for (let i = 0; i < this.currentWaybill.length; i++)//check if product exist in waybill
     {
-      
-      if (this.currentWaybill[i].product.id ==id) {
-         return this.currentWaybill[i]; 
+      if (this.currentWaybill[i].product.id == id) {
+        return this.currentWaybill[i];
       }
     }
-
     return null;
   }
-  getProducts()
-  {
-    this.productsService.getProducts(this.config.getProductsWithRelationalEntitiesUrl,null).subscribe(items=>{
-      this.productList=items;
+  getProducts() {
+    this.loading = true;
+    this.productsService.getProducts(this.config.getProductsWithRelationalEntitiesUrl, null).subscribe(items => {
+      this.productList = items;
       this.fillBasketProducts();
+      this.loading = false;
     });
   }
-  fillBasketProducts()
-  {
-    this.basketProducts=[];
-    
-     this.productList.forEach(element => {
-       let basketProduct=new BasketProduct();
-      
-      basketProduct.package=0;
-      let productInCurrentWaybill=this.getProductFromCurrentWaybill(element.id);
-      if(productInCurrentWaybill)
-      {
-        basketProduct.package=productInCurrentWaybill.package;
+  fillBasketProducts() {
+    this.basketProducts = [];
+
+    this.productList.forEach(element => {
+      let basketProduct = new BasketProduct();
+
+      basketProduct.package = 0;
+      let productInCurrentWaybill = this.getProductFromCurrentWaybill(element.id);
+      if (productInCurrentWaybill) {
+        basketProduct.package = productInCurrentWaybill.package;
       }
-      basketProduct.product=element;
+      basketProduct.product = element;
       this.basketProducts = [...this.basketProducts, basketProduct];
-     
-     });
-   
+    });
   }
 
-  saveWaybill()
-  {
-    let waybill:Waybill=new Waybill();
-    if(this.selectedWayBill!=null){
-    waybill.id=this.selectedWayBill.id;
-  }
-    waybill.addressId=this.selectedAddress.id;
-    waybill.customerId=this.selectedCustomer.id;
-    waybill.waybillDate=this.selectedDate;
-    waybill.waybillStatus=1;
-    waybill.isActive=true;
+  saveWaybill() {
+    let waybill: Waybill = new Waybill();
+    if (this.selectedWayBill != null) {
+      waybill.id = this.selectedWayBill.id;
+    }
+    waybill.addressId = this.selectedAddress.id;
+    waybill.customerId = this.selectedCustomer.id;
+    waybill.waybillDate = this.selectedDate;
+    waybill.waybillStatus = 1;
+    waybill.isActive = true;
     this.currentWaybill.forEach(basketProduct => {
-      let waybillProduct=new WaybillProduct();
-      waybillProduct.id=basketProduct.id;
-      waybillProduct.waybillId=basketProduct.waybillId;
-      waybillProduct.numberOfPackage=basketProduct.package;
-      waybillProduct.productId=basketProduct.product.id;
-      waybillProduct.status=basketProduct.status
+      let waybillProduct = new WaybillProduct();
+      waybillProduct.id = basketProduct.id;
+      waybillProduct.waybillId = basketProduct.waybillId;
+      waybillProduct.numberOfPackage = basketProduct.package;
+      waybillProduct.productId = basketProduct.product.id;
+      waybillProduct.status = basketProduct.status
       waybill.waybillProducts.push(waybillProduct);
     });
 
     //add also removed/deleted product with "deleted" flag/status
-    this.deletedBasketProducts.forEach (dltd=>{
-      let deletedWayBillProduct= new WaybillProduct();
-      deletedWayBillProduct.id=dltd.id;
-      deletedWayBillProduct.status=dltd.status;
+    this.deletedBasketProducts.forEach(dltd => {
+      let deletedWayBillProduct = new WaybillProduct();
+      deletedWayBillProduct.id = dltd.id;
+      deletedWayBillProduct.status = dltd.status;
       waybill.waybillProducts.push(deletedWayBillProduct);
 
-    }); 
+    });
 
-    this.waybillService.saveWaybill(this.config.saveWaybillUrl,waybill).subscribe(result=>{
+    this.waybillService.saveWaybill(this.config.saveWaybillUrl, waybill).subscribe(result => {
       this.toastr.info("irsaliye başarıyla kaydedildi...");
       this.removeCurrentWaybill();
-    //  this.setLastWaybill();
+      //  this.setLastWaybill();
     });
-    
-
   }
 
-  windowsHeight(){
-    return (window.screen.height -280) + "px";
+  windowsHeight() {
+    return (window.screen.height - 325) + "px";
   }
+  clearProducts() {
+    let dialogRef = this.dialog.open(ConfirmComponent, {
+      width: '50%',
+      data: { title: "irsaliyeyi sıfırlamak istiyor musunuz?" }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == 'yes') {
+        this.removeCurrentWaybill();
+      }
+    });
+  }
 }
